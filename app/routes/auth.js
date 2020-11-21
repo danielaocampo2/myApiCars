465e19cdbcf807e442d834ec7b03ce1f3d7d8c3c
daniela <danielaocampoo@gmail.com>
2020-11-21 00:04:51 -0500

    ruta login

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 //le vamos a dar al router algunas rutas //ejemplos
 router.post('/login', AuthCtrl.login)
-    .post('/loginToken', AuthCtrl.loginToken)
+    //.post('/loginToken', AuthCtrl.loginToken)
 
 module.exports = router;
