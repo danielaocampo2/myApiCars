57a5b088961b8fbcc7053696d56c5b7335fc24e1
daniela <danielaocampoo@gmail.com>
2020-11-20 23:05:50 -0500

    Cambio ruta

--- app/routes/owner.js
+++ app/routes/owner.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const UserCtrl = require('../controllers/OwnerController');
+const OwnerCtrl = require('../controllers/OwnerController');
 const routerU = express.Router();
 
 //le vamos a dar al router algunas rutas //ejemplos
-routerU.get('/showWorkers', UserCtrl.index) // api.com/user/  #Index: listar todos los users
-    .post('/', UserCtrl.create) // api.com/user/  #Create: crear un nuevo users
+routerU.get('/showOwners', OwnerCtrl.index) // api.com/user/  #Index: listar todos los users
+    .post('/', OwnerCtrl.create) // api.com/user/  #Create: crear un nuevo users
     //lo siguiente sera mandarle como un middleware prepararlo y se manda
-    .post('/refreshToken', UserCtrl.refreshtoken) // api.com/user/  #Create: crear un nuevo users
-    .get('/:key/:value', UserCtrl.find, UserCtrl.show) // api.com/user/category/Hogar #show: muestra un user en especifico
-    .put('/:key/:value', UserCtrl.find, UserCtrl.update) // api.com/user/name/SamsungGalaxy #update : actualizar un user en especifico
-    .delete('/:key/:value', UserCtrl.find, UserCtrl.remove) // api.com/user/name/SamsungGalaxy
+    .post('/refreshToken', OwnerCtrl.refreshtoken) // api.com/user/  #Create: crear un nuevo users
+    .get('/:key/:value', OwnerCtrl.find, OwnerCtrl.show) // api.com/user/category/Hogar #show: muestra un user en especifico
+    .put('/:key/:value', OwnerCtrl.find, OwnerCtrl.update) // api.com/user/name/SamsungGalaxy #update : actualizar un user en especifico
+    .delete('/:key/:value', OwnerCtrl.find, OwnerCtrl.remove) // api.com/user/name/SamsungGalaxy
 
 module.exports = routerU;
