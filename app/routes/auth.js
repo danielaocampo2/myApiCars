adf62dfbe6c0a275e24b66dce0b24bffa46ffa4c
daniela <danielaocampoo@gmail.com>
2020-11-20 23:50:40 -0500

    Error en la aplicación

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,7 +3,7 @@ const AuthCtrl = require('../controllers/AuthController')
 const router = express.Router();
 
 //le vamos a dar al router algunas rutas //ejemplos
-router.post('/login', AuthCtrl.login);
-router.post('/loginToken', AuthCtrl.loginToken);
+router.post('/login', AuthCtrl);
+router.post('/loginToken', AuthCtrl);
 
 module.exports = router;
