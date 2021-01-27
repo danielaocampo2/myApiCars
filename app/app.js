8633ffbbbb82d49aebdfa19d33627621e88d1f8e
daniela <danielaocampoo@gmail.com>
2021-01-27 11:05:39 -0500

    nuevo cors

--- app/app.js
+++ app/app.js
@@ -20,7 +20,7 @@ var corsOptions = {
             if (whitelist.indexOf(origin) !== -1) {
                 callback(null, true)
             } else {
-                callback(new Error('Not allowed by CORS'))
+                callback(new Error('holaaaaaaaaaaa Not allowed by CORS'))
             }
         }
     }
